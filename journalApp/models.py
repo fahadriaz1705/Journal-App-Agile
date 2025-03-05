ab1d3d86d0909cc3a9d05c9862a6beb7807da0ee
Rubaisha Nadeem <rubaisha.rn@gmail.com>
2025-03-05 22:39:03 +0500

    Applied code for customised themes

--- journalApp/models.py
+++ journalApp/models.py
@@ -33,9 +33,9 @@ class Attachment(models.Model):
 
 class Theme(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='theme')
-    primary_color = models.CharField(max_length=7, default="#000000")  # Default black
-    secondary_color = models.CharField(max_length=7, default="#FFFFFF")  # Default white
-    tertiary_color = models.CharField(max_length=7, default="#0000FF")  # Default blue
+    primary_color = models.CharField(max_length=7, default="#383940")  # Default black
+    secondary_color = models.CharField(max_length=7, default="#43545b")  # Default white
+    tertiary_color = models.CharField(max_length=7, default="#7b96a2")  # Default blue
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
