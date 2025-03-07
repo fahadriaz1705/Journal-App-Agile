3de8d378afe3502ea8ae7c4ebbee59410f7985f5
Rubaisha Nadeem <rubaisha.rn@gmail.com>
2025-03-08 03:06:22 +0500

    Missing Links fix + Theme Script File testing

--- journalApp/static/scripts/theme.js
+++
@@ -1,6 +0,0 @@
-let p_c = "{{user.theme.primary_color}}";
-let s_c = "{{user.theme.secondary_color}}"; 
-let t_c = "{{user.theme.tertiary_color}}";
-document.documentElement.style.setProperty('--primary_color', p_c);
-document.documentElement.style.setProperty('--secondary_color', s_c);
-document.documentElement.style.setProperty('--tertiary_color', t_c);
